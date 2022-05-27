a62dfc3e2be4f8891f261aec8849f81cb366c285
ErikW <enkiigm@gmail.com>
2022-05-27 18:02:55 +0200

    More refactoring

--- game.lua
+++ game.lua
@@ -1,4 +1,5 @@
 
+local util = require("util")
 local Grid = require("grid")
 
 
@@ -41,19 +42,16 @@ end
 
 function Game:gameOver()
     self.gamestate = "gameOver"
-    -- write score to high scores
     table.insert(self.highscores, self.score)
     self:saveHighscores()
 end
 
 
--- ASSUMPTION: the grid is not in a lose state
--- returns bool depending on whether the move was successful
 function Game:move(dir)
     local moveSuccessful = false
     if self.gamestate ~= "game" then return end
 
-    local moveF = "move" .. dir  -- hacky and brittle
+    local moveF = "move" .. dir  -- TODO: This is hacky and brittle
     
     local newGrid, mergeScore = self.curGrid[moveF](self.curGrid)
     
@@ -97,8 +95,6 @@ function Game:save(filename)
 end
 
 function Game:load(filename)
-    Game:initialize() -- Ensure everything is set TODO: Do this in main code instead?
-
     filename = filename or "save.dat"
 
     local saveStr = love.filesystem.read(filename)
@@ -118,7 +114,7 @@ function Game:load(filename)
     self.curGrid = self.history[#self.history].grid
     self:setScore(self.history[#self.history].score)
 
-    -- check if the game is over
+    
     if not self.curGrid:canMove() then
         self.gamestate = "gameOver"
     else
@@ -145,7 +141,6 @@ function Game:loadHighscores()
 end
 
 function Game:saveHighscores()
-    -- sort highscores
     table.sort(self.highscores, function(a, b) return a > b end)
 
     local highscoresStr = ""
@@ -155,5 +150,145 @@ function Game:saveHighscores()
 
     love.filesystem.write("highscores.dat", highscoresStr)
 end
+
+
+
+
+
+function Game:update(dt)
+    if Input:pressed("up") then
+        Game:move("Up")
+
+    elseif Input:pressed("down") then
+        Game:move("Down")
+    
+    elseif Input:pressed("left") then
+        Game:move("Left")
+    
+    elseif Input:pressed("right") then
+        Game:move("Right")
+    
+
+    elseif Input:pressed("restart") then
+        Game:restart()
+
+    elseif Input:pressed("undo") then
+        if Game.gamestate == "game" then
+            Game:undo()
+        end
+    
+    elseif Input:pressed("menuButton") then
+        if Game.gamestate == "game" then
+            Game.gamestate = "menu"
+
+        elseif Game.gamestate == "menu" then
+            Game.gamestate = "game"
+        
+        elseif Game.gamestate == "gameOver" then
+            Game:restart()
+        end
+    
+    elseif Input:pressed("quit") then
+        if Game.gamestate == "gameOver" or Game.gamestate == "menu" then
+            love.event.quit()
+
+        elseif Game.gamestate == "game" then
+            Game.gamestate = "menu"
+
+        end
+    end
+end
+
+
+
+
+local function drawGrid()
+    for x = 1, Grid_Size do
+        for y = 1, Grid_Size do
+            local tile = Game.curGrid[x][y]
+
+            if tile == 0 then
+                goto draw_grid_continue
+            end
+            
+            -- Draw the tile
+            -- calculate the hue of each tile based on the value, the hue is a 0-1 value depending on Max_Value_Power
+            -- This basically means there is a maximum power for the value of the tile, though it is not a hard limit
+            -- What happens to the hue beyond 2^Max_Value_Power I have no clue
+            local shade = (math.log(tile) / math.log(2)) / Max_Value_Power
+            love.graphics.setColor(util.hsvToRGB(shade, 0.8, 0.8))
+            love.graphics.rectangle("fill", (x - 1) * Tile_Size, (y - 1) * Tile_Size, Tile_Size, Tile_Size)
+        
+
+            -- draw the number
+            love.graphics.setColor(0, 0, 0)
+            local font = love.graphics.getFont()
+            local width = font:getWidth(tostring(tile))
+            local height = font:getHeight()
+            love.graphics.print(tostring(tile), (x - 1) * Tile_Size + (Tile_Size - width) / 2, (y - 1) * Tile_Size + (Tile_Size - height) / 2)
+
+            ::draw_grid_continue::
+        end
+    end
+end
+
+
+local function drawGameOver()
+    love.graphics.setColor(0.9, 0.9, 0.9, 0.70)
+    love.graphics.rectangle("fill", 0, 0, Window_Width, Window_Height)
+
+    love.graphics.setColor(0, 0, 0)
+    local font = love.graphics.getFont()
+    local height = font:getHeight()
+
+    love.graphics.print("Game Over", Window_Width / 2 - font:getWidth("Game Over") / 2, 20)
+    love.graphics.print("Your score: " .. Game.score, Window_Width / 2 - font:getWidth("Your score: " .. Game.score) / 2, height*1.2 + 20)
+
+    love.graphics.print("High Scores", Window_Width / 2 - font:getWidth("High Scores") / 2, 20 + height*1.2*2.5)
+    for i = 1, math.min(#Game.highscores, 4) do
+        love.graphics.print(Game.highscores[i], Window_Width / 2 - font:getWidth(Game.highscores[i]) / 2, 20 + height*1.2*2.5 + height*i)
+    end
+
+    love.graphics.print("Press Q to quit", Window_Width / 2 - font:getWidth("Press Q to quit") / 2, Window_Height - height*1.2*3)
+    love.graphics.print("Press R or Esc to restart", Window_Width / 2 - font:getWidth("Press R or Esc to restart") / 2, Window_Height - height*1.2*4)
+
+
+end
+
+
+local function drawMenu()
+    love.graphics.setColor(0.9, 0.9, 0.9, 0.70)
+    love.graphics.rectangle("fill", 0, 0, Window_Width, Window_Height)
+
+    love.graphics.setColor(0, 0, 0)
+    local font = love.graphics.getFont()
+    local height = font:getHeight()
+
+    love.graphics.print("2048 clone, by weakman54", Window_Width / 2 - font:getWidth("2048 clone, by weakman54") / 2, 20)
+
+    love.graphics.print("High Scores", Window_Width / 2 - font:getWidth("High Scores") / 2, 20 + height*1.2*1.5)
+    for i = 1, math.min(#Game.highscores, 4) do
+        love.graphics.print(Game.highscores[i], Window_Width / 2 - font:getWidth(Game.highscores[i]) / 2, 20 + height*1.2*1.5 + height*i)
+    end
+
+    love.graphics.print("Press Q to quit", Window_Width / 2 - font:getWidth("Press Q to quit") / 2, Window_Height - height*1.2*3)
+    love.graphics.print("Press R to restart", Window_Width / 2 - font:getWidth("Press R to restart") / 2, Window_Height - height*1.2*4)
+    love.graphics.print("Press Esc to go back to game", Window_Width / 2 - font:getWidth("Press Escape to go back to game") / 2, Window_Height - height*1.2*5)
+end
+
+
+function Game:draw()
+    drawGrid()
+    
+    if Game.gamestate == "gameOver" then
+        drawGameOver()
+    
+    elseif Game.gamestate == "menu" then
+        drawMenu()
+    end
+end
+
+
+
 
 return Game
--- main.lua
+++ main.lua
@@ -5,6 +5,10 @@
     figure out options for window
     "Seeded" randomness to make undo less cheaty (though it's still possible to use it to test for "favourable" moves)
     Fix Icon
+    Proper state management
+
+    Find a nice font
+    Redo menu screens to look nicer and be more consistent between each other
 
     Tests for score
     Tests for highscores
@@ -20,13 +24,8 @@
 require("global_config")
 
 local Game = require("game")
-local util = require("util")
 
 
-
-
-
--- Main ------------------------------------------------------------------------
 function love.load()
     love.window.setTitle(Title_Prefix)
     love.window.setMode(Window_Width, Window_Height, nil)
@@ -35,6 +34,7 @@ function love.load()
 
     love.graphics.setFont(love.graphics.newFont(24))
 
+    Game:initialize()
     Game:load()
 end
 
@@ -42,139 +42,12 @@ end
 function love.update(dt)
     Input:update()
 
-    if Input:pressed("up") then
-        Game:move("Up")
-
-    elseif Input:pressed("down") then
-        Game:move("Down")
-    
-    elseif Input:pressed("left") then
-        Game:move("Left")
-    
-    elseif Input:pressed("right") then
-        Game:move("Right")
-    
-
-    elseif Input:pressed("restart") then
-        Game:restart()
-
-    elseif Input:pressed("undo") then
-        if Game.gamestate == "game" then
-            Game:undo()
-        end
-    
-    elseif Input:pressed("menuButton") then
-        if Game.gamestate == "game" then
-            Game.gamestate = "menu"
-
-        elseif Game.gamestate == "menu" then
-            Game.gamestate = "game"
-        
-        elseif Game.gamestate == "gameOver" then
-            Game:restart()
-        end
-    
-    elseif Input:pressed("quit") then
-        if Game.gamestate == "gameOver" or Game.gamestate == "menu" then
-            love.event.quit()
-
-        elseif Game.gamestate == "game" then
-            Game.gamestate = "menu"
-
-        end
-    end
-end
-
-
-function love.keypressed(key)
-    -- if key == "f11" then
-    --     Tests()
-    -- end
-    
-    if key == "f12" then
-        
-        while not Game:hasLost() do
-            Game:move("Up")
-            Game:move("Down")
-            Game:move("Left")
-            Game:move("Right")
-        end
-    end
+    Game:update(dt)
 end
 
 
 function love.draw()
-    love.graphics.setColor(0.9, 0.9, 0.9)
-    
-
-    for x = 1, Grid_Size do
-        for y = 1, Grid_Size do
-            local tile = Game.curGrid[x][y]
-
-            if tile == 0 then
-                goto draw_grid_continue
-            end
-            
-            -- draw a rectangle for each tile, set the color to a hue depending on the value
-            -- given squared values, we can use the logarithm to get the hue
-            local shade = (math.log(tile) / math.log(2)) / Max_Value_Power
-            love.graphics.setColor(util.hsvToRGB(shade, 0.8, 0.8))
-            love.graphics.rectangle("fill", (x - 1) * Tile_Size, (y - 1) * Tile_Size, Tile_Size, Tile_Size)
-        
-            -- draw the number inside the tile
-            love.graphics.setColor(0, 0, 0)
-            local font = love.graphics.getFont()
-            local width = font:getWidth(tostring(tile))
-            local height = font:getHeight()
-            love.graphics.print(tostring(tile), (x - 1) * Tile_Size + (Tile_Size - width) / 2, (y - 1) * Tile_Size + (Tile_Size - height) / 2)
-            
-
-            ::draw_grid_continue::
-        end
-    end
-
-
-    -- Draw game over scren
-    if Game.gamestate == "gameOver" then
-        love.graphics.setColor(0.9, 0.9, 0.9, 0.70)
-        love.graphics.rectangle("fill", 0, 0, Window_Width, Window_Height)
-
-        love.graphics.setColor(0, 0, 0)
-        local font = love.graphics.getFont()
-        local height = font:getHeight()
-
-        love.graphics.print("Game Over", Window_Width / 2 - font:getWidth("Game Over") / 2, 20)
-        love.graphics.print("Score: " .. Game.score, Window_Width / 2 - font:getWidth("Score: " .. Game.score) / 2, height*1.2 + 20)
-        love.graphics.print("Press Q to quit", Window_Width / 2 - font:getWidth("Press Q to quit") / 2, Window_Height - height*1.2*3)
-        love.graphics.print("Press R or Esc to restart", Window_Width / 2 - font:getWidth("Press R or Esc to restart") / 2, Window_Height - height*1.2*4)
-
-        love.graphics.print("High Scores", Window_Width / 2 - font:getWidth("High Scores") / 2, 20 + height*1.2*2.5)
-        for i = 1, math.min(#Game.highscores, 4) do
-            love.graphics.print(Game.highscores[i], Window_Width / 2 - font:getWidth(Game.highscores[i]) / 2, 20 + height*1.2*2.5 + height*i)
-        end
-    end
-
-
-    -- Draw menu screen
-    if Game.gamestate == "menu" then
-        love.graphics.setColor(0.9, 0.9, 0.9, 0.70)
-        love.graphics.rectangle("fill", 0, 0, Window_Width, Window_Height)
-
-        love.graphics.setColor(0, 0, 0)
-        local font = love.graphics.getFont()
-        local height = font:getHeight()
-
-        love.graphics.print("2048 clone, by weakman54", Window_Width / 2 - font:getWidth("2048 clone, by weakman54") / 2, 20)
-
-        love.graphics.print("High Scores", Window_Width / 2 - font:getWidth("High Scores") / 2, 20 + height*1.2*1.5)
-        for i = 1, math.min(#Game.highscores, 4) do
-            love.graphics.print(Game.highscores[i], Window_Width / 2 - font:getWidth(Game.highscores[i]) / 2, 20 + height*1.2*1.5 + height*i)
-        end
-
-        love.graphics.print("Press Q to quit", Window_Width / 2 - font:getWidth("Press Q to quit") / 2, Window_Height - height*1.2*3)
-        love.graphics.print("Press R to restart", Window_Width / 2 - font:getWidth("Press R to restart") / 2, Window_Height - height*1.2*4)
-        love.graphics.print("Press Esc to go back to game", Window_Width / 2 - font:getWidth("Press Escape to go back to game") / 2, Window_Height - height*1.2*5)
-    end
+    Game:draw()
 end
 
 
